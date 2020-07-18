a3ab1c14e1cc3d590a75f5a29ca97408abfe3485
mikerueda <mareacreativo@gmail.com>
2020-07-18 12:01:50 -0300

    cv migrated

---
+++ components/ExperiencesList.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const ExperienceItem = ({ data }) => {
+	return (
+		<li className="experienceItem">
+			<article>
+				<h4>
+					<strong>{data.company} </strong>
+					<span>{data.location}</span>
+				</h4>
+				<h5>
+					<strong>{data.role} </strong>
+					<span>{data.date}</span>
+				</h5>
+				{data.description.map((desc, index) => <p key={index}>{desc}</p>)}
+			</article>
+		</li>
+	)
+}
+
+export default ({experiences}) => {
+	return (
+		<ul>
+			{experiences.map((exp, index) => (
+				// Pass a index as key for render pourposes
+				<ExperienceItem key={index} data={exp} />
+			))}
+		</ul>
+	)
+}
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,33 @@
-import HelloWorld from '../components/hello-world'
+import React, { useEffect, useState } from 'react'
+import SideBar from '../components/SideBar'
+import Content from '../components/Content'
+import axios from 'axios'
 
-export default function Home() {
-  return (
-    <div className="app">
-      <HelloWorld />
-    </div>
-  )
+const fetchHeader = {
+	headers: {
+		'Content-Type': 'application/json',
+		Accept: 'application/json',
+		'secret-key': '$2b$10$5nuQcJ7ZfFm7ND3hL59pnOtCwE8UyDerAhTOcB86u2V.zqnX2QKLS'
+	}
 }
+
+const Home = ({experiences}) => {
+
+	return (
+		<div className="container">
+			<SideBar />
+			<Content experiences={experiences}/>
+		</div>
+	)
+}
+
+Home.getInitialProps = async () => {
+  const response = await axios.get('https://api.jsonbin.io/b/5f124cc6c1edc4661759241f', fetchHeader)
+  return {
+    experiences: response.data
+  }
+
+}
+
+
+export default Home
